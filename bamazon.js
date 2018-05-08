0f8f2dabcc98e03e5902f9141aefb2088ab9276c
sls2u <sarahstewartvsp@gmail.com>
2018-05-08 17:26:33 -0400

    start prompt

--- bamazon.js
+++ bamazon.js
@@ -1,9 +1,41 @@
+const mysql = require('mysql');
 const inquirer = require('inquirer');
-// 
-// return inquirer.prompt
-// ([
-//   type:'command',
-// 
-// ])
-// 
-console.log('hello');
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Welcome123",
+  database: "bamazon"
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected');
+  makeTable();
+});
+
+let makeTable = function() {
+  connection.query("select *from products", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + res[i].price + "|| " + res[i].stock_quantity + "\n");
+    }
+    promptCustomer(res);
+  });
+}
+let promptCustomer = function(res) {
+  inquirer.prompt([{
+    name: "item",
+    type: "input",
+    message: "What Item would you like to buy?"
+  }]).then(function(answer) {
+    let correct = false;
+    for (var i = 0; i < res.length; i++) {
+      correct === true;
+      let product = answer.choice;
+      let id = i;
+      inquirer.prompt({
+
+      })
+    }
+  })
+}
